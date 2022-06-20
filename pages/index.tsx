928cc1d726f24dfddd391f47c90b6147d2073244
german alvarez garcia <germanalvarez14@gmail.com>
2022-06-19 23:14:16 -0700

    setup mantine

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,30 @@
 import type { NextPage } from "next";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "../components/nav";
+
+const links = [
+  {
+    link: "/home",
+    label: "Features",
+  },
+  {
+    link: "/services",
+    label: "Sevices",
+  },
+  {
+    link: "/mentoringNetwork",
+    label: "Mentoring Network",
+  },
+  {
+    link: "/freeResources",
+    label: "Free Resources",
+  },
+];
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session!.user?.email} <br />
-        <button className="btn btn-primary" onClick={() => signOut()}>
-          Sign out
-        </button>
-      </>
-    );
-  }
   return (
     <>
-      <button className="btn btn-primary"> click me</button>
-      Not signed in <br />
-      <button className="btn btn-primary" onClick={() => signIn()}>
-        Sign in
-      </button>
+      <Nav links={links} />
     </>
   );
 };
